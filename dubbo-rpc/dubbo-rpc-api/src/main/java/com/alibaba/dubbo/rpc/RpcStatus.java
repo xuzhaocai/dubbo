2795925ac00be505bf521212f5297e6907929926
xuzhaocai <1193547899@qq.com>
2020-08-09 17:34:07 +0800

    ActiveLimitFilter 添加注释

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/RpcStatus.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/RpcStatus.java
@@ -33,19 +33,21 @@ import java.util.concurrent.atomic.AtomicLong;
  * @see com.alibaba.dubbo.rpc.cluster.loadbalance.LeastActiveLoadBalance
  */
 public class RpcStatus {
-
+    // 这个SERVICE_STATISTICS 是缓存着service统计信息
     private static final ConcurrentMap<String, RpcStatus> SERVICE_STATISTICS = new ConcurrentHashMap<String, RpcStatus>();
-
+    // 这个 METHOD_STATISTICS 缓存着 service 方法级别的统计信息
     private static final ConcurrentMap<String, ConcurrentMap<String, RpcStatus>> METHOD_STATISTICS = new ConcurrentHashMap<String, ConcurrentMap<String, RpcStatus>>();
     private final ConcurrentMap<String, Object> values = new ConcurrentHashMap<String, Object>();
-    private final AtomicInteger active = new AtomicInteger();
-    private final AtomicLong total = new AtomicLong();
-    private final AtomicInteger failed = new AtomicInteger();
-    private final AtomicLong totalElapsed = new AtomicLong();
-    private final AtomicLong failedElapsed = new AtomicLong();
-    private final AtomicLong maxElapsed = new AtomicLong();
-    private final AtomicLong failedMaxElapsed = new AtomicLong();
-    private final AtomicLong succeededMaxElapsed = new AtomicLong();
+
+
+    private final AtomicInteger active = new AtomicInteger();// 记录当前调用并发的， 这个是会实时改的
+    private final AtomicLong total = new AtomicLong(); // 总调用次数
+    private final AtomicInteger failed = new AtomicInteger();// 失败次数
+    private final AtomicLong totalElapsed = new AtomicLong();// 总消耗时间
+    private final AtomicLong failedElapsed = new AtomicLong();// 失败消耗时间
+    private final AtomicLong maxElapsed = new AtomicLong();// 最大消耗时间
+    private final AtomicLong failedMaxElapsed = new AtomicLong();// 失败最大消耗时间
+    private final AtomicLong succeededMaxElapsed = new AtomicLong();// 成功最大消耗时间
 
     /**
      * Semaphore used to control concurrency limit set by `executes`
@@ -61,9 +63,11 @@ public class RpcStatus {
      * @return status
      */
     public static RpcStatus getStatus(URL url) {
-        String uri = url.toIdentityString();
+        // 生成IdentityString
+        String uri = url.toIdentityString();//dubbo://192.168.3.33:18109/com.xuzhaocai.dubbo.provider.IHelloProviderService
+        //获取IdentityString对应的缓存
         RpcStatus status = SERVICE_STATISTICS.get(uri);
-        if (status == null) {
+        if (status == null) {// 没有就生成 塞到SERVICE_STATISTICS
             SERVICE_STATISTICS.putIfAbsent(uri, new RpcStatus());
             status = SERVICE_STATISTICS.get(uri);
         }
@@ -79,22 +83,32 @@ public class RpcStatus {
     }
 
     /**
+     * 获取 url对应缓存 method的RpcStatus
      * @param url
      * @param methodName
      * @return status
      */
     public static RpcStatus getStatus(URL url, String methodName) {
-        String uri = url.toIdentityString();
+        String uri = url.toIdentityString();// 生成IdentityString   dubbo://192.168.3.33:18109/com.xuzhaocai.dubbo.provider.IHelloProviderService
+
+        // 使用IdentityString  获取
+
+        // key是方法名，value是RpcStatus
         ConcurrentMap<String, RpcStatus> map = METHOD_STATISTICS.get(uri);
-        if (map == null) {
+        if (map == null) {// 没有就创建塞进去
             METHOD_STATISTICS.putIfAbsent(uri, new ConcurrentHashMap<String, RpcStatus>());
             map = METHOD_STATISTICS.get(uri);
         }
+
+        // 获取methodName 对应的RpcStatus
         RpcStatus status = map.get(methodName);
+
+        // 没有就创建塞进去
         if (status == null) {
             map.putIfAbsent(methodName, new RpcStatus());
             status = map.get(methodName);
         }
+        // 返回method 对应的 RpcStatus
         return status;
     }
 
@@ -110,6 +124,7 @@ public class RpcStatus {
     }
 
     /**
+     *
      * @param url
      */
     public static void beginCount(URL url, String methodName) {
@@ -118,8 +133,10 @@ public class RpcStatus {
     }
 
     private static void beginCount(RpcStatus status) {
-        status.active.incrementAndGet();
+        status.active.incrementAndGet();// active+1
     }
+
+
 
     /**
      * @param url
@@ -127,24 +144,37 @@ public class RpcStatus {
      * @param succeeded
      */
     public static void endCount(URL url, String methodName, long elapsed, boolean succeeded) {
+
+
         endCount(getStatus(url), elapsed, succeeded);
         endCount(getStatus(url, methodName), elapsed, succeeded);
     }
 
     private static void endCount(RpcStatus status, long elapsed, boolean succeeded) {
+
+        // actives -1
         status.active.decrementAndGet();
+        // total +1
         status.total.incrementAndGet();
+        // 累加 消耗时间
         status.totalElapsed.addAndGet(elapsed);
-        if (status.maxElapsed.get() < elapsed) {
-            status.maxElapsed.set(elapsed);
+        // 记录最大消耗时间
+        if (status.maxElapsed.get() < elapsed) {// 本次消耗时间 大于 之前记录的最大消耗时间
+            status.maxElapsed.set(elapsed);// 记录最大消耗时间
         }
-        if (succeeded) {
+        if (succeeded) {//成功的话
+
+            // 记录成功最大消耗时间
             if (status.succeededMaxElapsed.get() < elapsed) {
                 status.succeededMaxElapsed.set(elapsed);
             }
-        } else {
+        } else {// 失败的话
+
+            // 失败次数+1
             status.failed.incrementAndGet();
+            //  累加失败消耗时间
             status.failedElapsed.addAndGet(elapsed);
+            // 记录失败最大消耗时间
             if (status.failedMaxElapsed.get() < elapsed) {
                 status.failedMaxElapsed.set(elapsed);
             }
--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/ActiveLimitFilter.java
@@ -28,31 +28,47 @@ import com.alibaba.dubbo.rpc.RpcStatus;
 
 /**
  * LimitInvokerFilter
+ * consumer actives
  */
 @Activate(group = Constants.CONSUMER, value = Constants.ACTIVES_KEY)
 public class ActiveLimitFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        // 获取url
         URL url = invoker.getUrl();
+        // 获取调用方法名
         String methodName = invocation.getMethodName();
+        // 获取方法 actives 属性值 缺省是0 ，这actives 就是"每服务消费者每服务每方法最大并发调用数"
         int max = invoker.getUrl().getMethodParameter(methodName, Constants.ACTIVES_KEY, 0);
+        // 获取对应url 对应method的一个RpcStatus
         RpcStatus count = RpcStatus.getStatus(invoker.getUrl(), invocation.getMethodName());
         if (max > 0) {
+            // 获取timeout
             long timeout = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.TIMEOUT_KEY, 0);
+            // 开始时间
             long start = System.currentTimeMillis();
+            //剩余时间= timeout
             long remain = timeout;
+
+            /// 获取一个active值
             int active = count.getActive();
+
+            // 当active 大于max，说明调用的已经超了设置最大并发数
             if (active >= max) {
                 synchronized (count) {
+                    // 活跃着 的还是大于 最大并发数
                     while ((active = count.getActive()) >= max) {
                         try {
+                            // 进行等待  等待超时时间
                             count.wait(remain);
                         } catch (InterruptedException e) {
                         }
+                        // 计算消耗多少时间
                         long elapsed = System.currentTimeMillis() - start;
+                        // 剩余时间  超时时间-消耗时间
                         remain = timeout - elapsed;
-                        if (remain <= 0) {
+                        if (remain <= 0) {// 小于等于0，说明没有剩余时间了，也就是超时了 ，这里直接抛出超时
                             throw new RpcException("Waiting concurrent invoke timeout in client-side for service:  "
                                     + invoker.getInterface().getName() + ", method: "
                                     + invocation.getMethodName() + ", elapsed: " + elapsed
@@ -64,17 +80,24 @@ public class ActiveLimitFilter implements Filter {
             }
         }
         try {
+            // 开始时间
             long begin = System.currentTimeMillis();
+            // 对应的RpcStatus 的 actives+1
             RpcStatus.beginCount(url, methodName);
             try {
+                // 进行调用
                 Result result = invoker.invoke(invocation);
+
                 RpcStatus.endCount(url, methodName, System.currentTimeMillis() - begin, true);
                 return result;
             } catch (RuntimeException t) {
+
+                // 失败情况
                 RpcStatus.endCount(url, methodName, System.currentTimeMillis() - begin, false);
                 throw t;
             }
         } finally {
+            // 执行完唤醒
             if (max > 0) {
                 synchronized (count) {
                     count.notify();
